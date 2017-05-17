0c57444a41b7dc18c68ae7e54494f41c8ee2a8c0
RAJVEER <RAJVEER@RAJVEER-PC>
2017-05-17 14:00:07 +0530

    bs

---
+++ src/bean/scope/Restorent.java
@@ -0,0 +1,14 @@
+package bean.scope;
+
+public class Restorent {
+	public String welcomeNote;
+
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
+		
+	}
+
+	public void greedCustomer() {
+		System.out.println(welcomeNote);
+	}
+}
